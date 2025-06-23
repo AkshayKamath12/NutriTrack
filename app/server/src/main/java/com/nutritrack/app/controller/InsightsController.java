e364cde9955cf4c52a464487bbb2de5739d1a80b
Akshay Kamath <akkamath@ucsc.edu>
2025-06-23 16:07:59 -0700

    adding insights controller

--- app/server/src/main/java/com/nutritrack/app/controller/InsightsController.java
+++ app/server/src/main/java/com/nutritrack/app/controller/InsightsController.java
@@ -1,4 +1,25 @@
 package com.nutritrack.app.controller;
 
+import com.nutritrack.app.dto.SleepInsightsDTO;
+import com.nutritrack.app.service.InsightsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/insights")
 public class InsightsController {
+    private InsightsService insightsService;
+
+    public InsightsController(InsightsService insightsService) {
+        this.insightsService = insightsService;
+    }
+
+    @GetMapping("/sleep")
+    public SleepInsightsDTO sleepInsights(Principal principal) {
+        String username = principal.getName();
+        return insightsService.getSleepInsights(username);
+    }
 }
