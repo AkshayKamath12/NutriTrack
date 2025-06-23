63339d306c304cc34aa9addf8e028abdd551392a
Akshay Kamath <akkamath@ucsc.edu>
2025-06-23 16:09:46 -0700

    adding overall insights service that uses sleep insights service

--- app/server/src/main/java/com/nutritrack/app/service/InsightsService.java
+++ app/server/src/main/java/com/nutritrack/app/service/InsightsService.java
@@ -1,4 +1,23 @@
 package com.nutritrack.app.service;
 
+import com.nutritrack.app.dto.SleepInsightsDTO;
+import com.nutritrack.app.dto.sleep.SleepNumeric;
+import com.nutritrack.app.dto.sleep.SleepText;
+import com.nutritrack.app.entity.NutriLabel;
+import com.nutritrack.app.service.insights.SleepInsightsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InsightsService {
+    private SleepInsightsService sleepInsightsService;
+
+    public InsightsService(SleepInsightsService sleepInsightsService) {
+        this.sleepInsightsService = sleepInsightsService;
+    }
+
+    public SleepInsightsDTO getSleepInsights(String username) {
+        return sleepInsightsService.getSleepInsights(username);
+    }
 }
