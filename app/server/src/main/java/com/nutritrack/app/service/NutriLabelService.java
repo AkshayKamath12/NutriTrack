f6248c8292bfed8ab67267d828c22be1895b36e7
Akshay Kamath <akkamath@ucsc.edu>
2025-06-24 09:58:59 -0700

    updated service to use meals and set bidirectional relationship

--- app/server/src/main/java/com/nutritrack/app/service/NutriLabelService.java
+++ app/server/src/main/java/com/nutritrack/app/service/NutriLabelService.java
@@ -1,6 +1,8 @@
 package com.nutritrack.app.service;
 
+import com.nutritrack.app.dao.MealRepository;
 import com.nutritrack.app.dao.NutriLabelRepository;
+import com.nutritrack.app.entity.Meal;
 import com.nutritrack.app.entity.NutriLabel;
 import com.nutritrack.app.entity.User;
 import org.springframework.stereotype.Service;
@@ -10,26 +12,23 @@ import java.util.Optional;
 
 @Service
 public class NutriLabelService {
+    private final MealRepository mealRepository;
     private NutriLabelRepository nutriLabelRepository;
-    private UserService userService;
 
-    public NutriLabelService(NutriLabelRepository nutriLabelRepository, UserService userService) {
+    public NutriLabelService(NutriLabelRepository nutriLabelRepository, MealRepository mealRepository) {
         this.nutriLabelRepository = nutriLabelRepository;
-        this.userService = userService;
+        this.mealRepository = mealRepository;
     }
 
-    public List<NutriLabel> getAllNutriLabels(String username) {
-        return nutriLabelRepository.findByUserUsername(username);
+    public List<NutriLabel> getAllNutriLabels(Meal meal) {
+        return meal.getNutritionLabels();
     }
 
-    public void addNutriLabel(NutriLabel nutriLabel, String username) {
-        Optional<User> user= userService.findByUsername(username);
-        if(user.isEmpty()) {
-            return;
-        }
-        User nutriLabelUser = user.get();
-        nutriLabel.setUser(nutriLabelUser);
+    public void addNutriLabel(NutriLabel nutriLabel, Meal meal) {
+        nutriLabel.setMeal(meal);
         nutriLabelRepository.save(nutriLabel);
+        meal.getNutritionLabels().add(nutriLabel);
+        mealRepository.save(meal);
     }
 
     public boolean updateNutriLabel(NutriLabel updatedLabel, long id) {
