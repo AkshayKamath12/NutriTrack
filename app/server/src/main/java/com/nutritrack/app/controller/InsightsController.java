8b8e53764e069ff1ab6ac2b5dfa045db9bd3415b
Akshay Kamath <akkamath@ucsc.edu>
2025-06-24 09:57:08 -0700

    tying insights service to meal to get labels for tracking

--- app/server/src/main/java/com/nutritrack/app/controller/InsightsController.java
+++ app/server/src/main/java/com/nutritrack/app/controller/InsightsController.java
@@ -1,25 +1,32 @@
 package com.nutritrack.app.controller;
 
 import com.nutritrack.app.dto.SleepInsightsDTO;
+import com.nutritrack.app.entity.Meal;
 import com.nutritrack.app.service.InsightsService;
+import com.nutritrack.app.service.MealService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/insights")
 public class InsightsController {
     private InsightsService insightsService;
+    private MealService mealService;
 
-    public InsightsController(InsightsService insightsService) {
+    public InsightsController(InsightsService insightsService, MealService mealService) {
         this.insightsService = insightsService;
+        this.mealService = mealService;
     }
 
-    @GetMapping("/sleep")
-    public SleepInsightsDTO sleepInsights(Principal principal) {
-        String username = principal.getName();
-        return insightsService.getSleepInsights(username);
+    @GetMapping("/sleep/{mealId}")
+    public SleepInsightsDTO sleepInsights(@PathVariable long mealId) {
+        Meal meal = mealService.getMeal(mealId);
+        if (meal == null) {
+            return null;
+        }
+        return insightsService.getSleepInsights(meal);
     }
 }
