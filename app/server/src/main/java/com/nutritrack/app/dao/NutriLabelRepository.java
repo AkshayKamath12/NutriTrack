28873c98110668af7d6d46ee204476fcf4c8ea43
Akshay Kamath <akkamath@ucsc.edu>
2025-06-24 09:52:20 -0700

    no longer need method since meals are now tied to user instead

--- app/server/src/main/java/com/nutritrack/app/dao/NutriLabelRepository.java
+++ app/server/src/main/java/com/nutritrack/app/dao/NutriLabelRepository.java
@@ -3,8 +3,5 @@ package com.nutritrack.app.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.nutritrack.app.entity.NutriLabel;
 
-import java.util.List;
-
 public interface NutriLabelRepository extends JpaRepository<NutriLabel, Long> {
-    List<NutriLabel> findByUserUsername(String username);
 }
