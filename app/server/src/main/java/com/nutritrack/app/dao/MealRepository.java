1322b4ed24e81ac7b8754b3a9da30ba3a4204e16
Akshay Kamath <akkamath@ucsc.edu>
2025-06-24 09:53:27 -0700

    adding jpa repository with method to get meals by username

--- app/server/src/main/java/com/nutritrack/app/dao/MealRepository.java
+++ app/server/src/main/java/com/nutritrack/app/dao/MealRepository.java
@@ -1,4 +1,10 @@
 package com.nutritrack.app.dao;
 
-public interface MealRepository {
+import com.nutritrack.app.entity.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MealRepository extends JpaRepository<Meal, Long> {
+    public List<Meal> findByUserUsername(String username);
 }
