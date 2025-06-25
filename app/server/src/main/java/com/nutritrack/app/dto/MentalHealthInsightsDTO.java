9d77edf77472e09efb79b4a4e566ea82a8ac69f0
Akshay Kamath <akkamath@ucsc.edu>
2025-06-25 13:33:55 -0700

    DTO wrapper for mental health insights

---
+++ app/server/src/main/java/com/nutritrack/app/dto/MentalHealthInsightsDTO.java
@@ -0,0 +1,13 @@
+package com.nutritrack.app.dto;
+
+import com.nutritrack.app.dto.mentalHealth.MentalHealthNumeric;
+import com.nutritrack.app.dto.mentalHealth.MentalHealthScores;
+import com.nutritrack.app.dto.mentalHealth.MentalHealthText;
+import lombok.Data;
+
+@Data
+public class MentalHealthInsightsDTO {
+    private MentalHealthNumeric mentalHealthNumeric;
+    private MentalHealthText mentalHealthText;
+    private MentalHealthScores mentalHealthScores;
+}
