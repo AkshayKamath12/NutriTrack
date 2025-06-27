dc6d95f89f05371e2d0ff427cfcc7aeec8d0e8aa
Akshay Kamath <akkamath@ucsc.edu>
2025-06-27 16:44:41 -0700

    Implementing overall rate limiting using bucket4j

---
+++ app/server/src/main/java/com/nutritrack/app/security/RateLimiter.java
@@ -0,0 +1,14 @@
+package com.nutritrack.app.security;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+
+import java.time.Duration;
+
+public class RateLimiter {
+    public static Bucket createNewBucket() {
+        Bandwidth limit = Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+}
